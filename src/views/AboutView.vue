c9d1a3f97a36241f08826eb842269bd53fcfde15
jyko <yeon0125@gmail.com>
2023-10-16 23:19:26 +0900

    [css] setting, test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-xl font-medium text-white">This is an about page</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<style lang="scss">
+.about {
+  @apply lg:min-h-screen lg:flex lg:items-center;
+  h1 {
+    @apply text-xl font-medium text-white;
   }
 }
 </style>
